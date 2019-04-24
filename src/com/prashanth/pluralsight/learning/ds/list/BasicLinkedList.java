f351aadd223f6754d2dc3259bcc45abc0098dcb1
prashanth-g <prashanthcsgce@gmail.com>
2019-04-24 22:17:21 +0530

    PS Learning - Added insert and removeAt methods

--- src/com/prashanth/pluralsight/learning/ds/list/BasicLinkedList.java
+++ src/com/prashanth/pluralsight/learning/ds/list/BasicLinkedList.java
@@ -28,6 +28,46 @@ public class BasicLinkedList<T> {
         nodeCount++;
     }
 
+    public void insert(T item, int position) {
+        if(size() < position) {
+            throw new IllegalArgumentException("Position not found");
+        }
+
+        Node currentNode = first;
+
+        for (int i = 1; i < position && currentNode != null; i++) {
+            currentNode = currentNode.getNextNode();
+        }
+
+        Node newNode = new Node(item);
+        Node nextNode = currentNode.getNextNode();
+
+        currentNode.setNextNode(newNode);
+        newNode.setNextNode(nextNode);
+
+        nodeCount++;
+    }
+
+    public T removeAt(int position) {
+        if(first == null) {
+            throw new IllegalStateException("LinkedList is empty, there is no item to remove");
+        }
+
+        Node currentNode = first;
+        Node prevNode = first;
+
+        for (int i = 1; i < position && currentNode != null; i++) {
+            prevNode = currentNode;
+            currentNode = currentNode.getNextNode();
+        }
+
+        // Get next to next of current  - S
+        T nodeItem = currentNode.getNodeItem();
+        prevNode.setNextNode(currentNode.getNextNode());
+        nodeCount--;
+        return nodeItem;
+    }
+
     public T remove() {
         if(first == null) {
             throw new IllegalArgumentException("LinkedList is empty!");
@@ -39,6 +79,59 @@ public class BasicLinkedList<T> {
             nodeCount--;
             return nodeItem;
         }
+    }
+
+    public T get(int position) {
+        if(size() < position) {
+            throw new IllegalArgumentException("Position not found");
+        }
+
+        Node currentNode = first;
+
+        for (int i = 1; i < position && currentNode != null; i++) {
+            if(i == position) {
+                return currentNode.getNodeItem();
+            }
+            currentNode = currentNode.getNextNode();
+        }
+
+        return null;
+    }
+
+    // 4, 3, 6
+    // 0, 1, 2
+    public int findItem(T item) {
+        if (first == null) {
+            throw new IllegalArgumentException("LinkedList is empty");
+        }
+
+        Node currentNode = first;
+
+
+        for (int i = 1; i < size() && currentNode != null; i++) {
+            if(currentNode.getNodeItem() == item) {
+                return i;
+            }
+            currentNode = currentNode.getNextNode();
+        }
+
+        return -1;
+    }
+
+    public String toString() {
+        StringBuffer stringBuffer = new StringBuffer();
+        Node currentNode = first;
+
+        while(currentNode != null) {
+            stringBuffer.append(currentNode.getNodeItem());
+            currentNode = currentNode.getNextNode();
+
+            // If next is not null
+            if(currentNode != null) {
+                stringBuffer.append(",");
+            }
+        }
+        return stringBuffer.toString();
     }
 
     private class Node {
