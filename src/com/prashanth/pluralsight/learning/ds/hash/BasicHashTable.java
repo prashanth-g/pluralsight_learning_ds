0b697e78f51b9985c7e3347232d536615b97ec9f
prashanth-g <prashanthcsgce@gmail.com>
2019-04-28 11:30:01 +0530

    PS Learning - Added HashTable Functions

--- src/com/prashanth/pluralsight/learning/ds/hash/BasicHashTable.java
+++ src/com/prashanth/pluralsight/learning/ds/hash/BasicHashTable.java
@@ -4,10 +4,82 @@ public class BasicHashTable<K, V> {
 
     private HashEntry[] data;
     private int capacity;
+    private int size;
 
     public BasicHashTable(int tableSize) {
         this.capacity = capacity;
         this.data = new HashEntry[this.capacity];
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public V get(K key) {
+        int hash = calculateHash(key);
+
+        if(data[hash] == null) {
+            return null;
+        } else {
+            return (V) data[hash].getValue();
+        }
+    }
+
+    public void put(K key, V value) {
+        int hash = calculateHash(key);
+
+        data[hash] = new HashEntry<K, V>(key, value);
+        size++;
+    }
+
+    public V delete(K key) {
+        // Get the value
+        V value = get(key);
+        // If the value is not null - We need to clear it out
+        if(value != null) {
+            int hash = calculateHash(key);
+            data[hash] = null;
+            size--;
+            hash = (hash + 1) % this.capacity;
+
+            while(data[hash] != null) {
+                HashEntry he = data[hash];
+                data[hash] = null;
+                put((K) he.getKey(), (V) he.getValue());
+                size--;
+                hash = (hash + 1) % this.capacity;
+            }
+        }
+        return value;
+    }
+
+    public boolean hasKey(K key) {
+        int hash = calculateHash(key);
+        if(data[hash] != null) {
+            if(data[hash].getKey() == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasValue(V value) {
+        for (int i = 0; i < this.capacity; i++) {
+            HashEntry he = data[i];
+            if(he != null && he.getValue().equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int calculateHash(K key) {
+        int hash = (key.hashCode() % this.capacity);
+        while(data[hash] != null && !data[hash].getKey().equals(key)) {
+            hash = (hash + 1) % this.capacity;
+        }
+        return hash;
     }
 
     private class HashEntry<K, V> {
