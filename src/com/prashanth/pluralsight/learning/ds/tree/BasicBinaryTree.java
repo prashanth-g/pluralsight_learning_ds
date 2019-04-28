5568492fdbcc0a4630d8b620206d5a185dd1fa23
prashanth-g <prashanthcsgce@gmail.com>
2019-04-28 14:39:33 +0530

    PS Learning - Added BasicBinarySearchTree with basic functions

---
+++ src/com/prashanth/pluralsight/learning/ds/tree/BasicBinaryTree.java
@@ -0,0 +1,143 @@
+package com.prashanth.pluralsight.learning.ds.tree;
+
+public class BasicBinaryTree<T extends Comparable<T>> {
+
+    private Node root;
+    private int size;
+
+    public BasicBinaryTree() {
+        this.root = null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(T item) {
+       Node newNode = new Node(item);
+
+       if(root == null) {
+           root = newNode;
+           this.size++;
+       } else {
+           insert(this.root, newNode);
+       }
+    }
+
+    private void insert(Node parent, Node child) {
+        if(child.getItem().compareTo(parent.getItem()) < 0) {
+            if(parent.getItem() == null) {
+                parent.setLeft(child);
+                child.setParent(parent);
+                this.size++;
+            } else {
+                insert(parent.getLeft(), child);
+            }
+        } else if(child.getItem().compareTo(parent.getItem()) > 0) {
+            if(parent.getItem() == null) {
+                parent.setRight(child);
+                child.setParent(parent);
+                this.size++;
+            } else {
+                insert(parent.getRight(), child);
+            }
+        }
+
+        // Assuming data in binary in unique so leaving == 0 condition
+    }
+
+    public boolean contains(T item) {
+       if(getNode(item) != null) {
+           return true;
+       }
+       return false;
+    }
+
+    private Node getNode(T item) {
+        Node currentNode = this.root;
+        while(currentNode != null) {
+            int val = item.compareTo(currentNode.getItem());
+            if(val == 0) {
+                return currentNode;
+            }
+            else if(val < 0) {
+                currentNode = currentNode.getLeft();
+            } else {
+                currentNode = currentNode.getRight();
+            }
+        }
+
+        return null;
+    }
+
+    // Recursive
+//    private boolean find(Node parent, T item) {
+//        if(parent.getItem().equals(item)) {
+//            return true;
+//        } else {
+//            if(item.compareTo(parent.getItem()) < 0) {
+//                if(parent.getItem() == null) {
+//                    parent = parent.getLeft();
+//                    find(parent, item);
+//                } else {
+//                    return false;
+//                }
+//            } else if(item.compareTo(parent.getItem()) > 0) {
+//                if(parent.getItem() == null) {
+//                    parent = parent.getRight();
+//                    find(parent, item);
+//                } else {
+//                    return false;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
+    private class Node {
+
+        private Node left;
+        private Node right;
+        private Node parent;
+        private T item;
+
+        public Node(T item) {
+            this.left = null;
+            this.right = null;
+            this.parent = null;
+            this.item = item;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Node getParent() {
+            return parent;
+        }
+
+        public void setParent(Node parent) {
+            this.parent = parent;
+        }
+
+        public T getItem() {
+            return item;
+        }
+
+        public void setItem(T item) {
+            this.item = item;
+        }
+    }
+}
